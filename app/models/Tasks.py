276cbca394c2ae0e94848c7b440b8220b98086a0
keavy707 <a.keavyocay@gmail.com>
2026-04-16 14:21:40 +0800

    upd

--- app/models/Tasks.py
+++ app/models/Tasks.py
@@ -3,3 +3,5 @@ from sqlmodel import Field, SQLModel
 
 class Tasks(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    description: str | None = Field(default=None)
